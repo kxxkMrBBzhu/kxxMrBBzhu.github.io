f3b80fa7ce99e46769148e2b6efd3e71b3e59cbf
kxxkMrBBzhu <531964016@qq.com>
2025-04-22 00:49:19 +0800

    add web

---
+++ script.js
@@ -0,0 +1,5 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+document.querySelector("html").addEventListener("click", () => {
+    alert("别戳我，我怕疼！");
+});
