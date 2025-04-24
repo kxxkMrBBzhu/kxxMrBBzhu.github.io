f2d30edcd4b37cceb867fd71a6b888eb3bdfceb0
kxxkMrBBzhu <531964016@qq.com>
2025-04-24 23:26:43 +0800

    delete

--- script.js
+++
@@ -1,5 +0,0 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-document.querySelector("html").addEventListener("click", () => {
-    alert("别戳我，我怕疼！");
-});
